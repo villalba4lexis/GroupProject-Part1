4e831371e8201f71abfc4a7a10effdd770c8762e
Alexis Arenas-Villalba <alexisa.villalba05@icloud.com>
2025-07-22 20:30:24 -0400

    Code Skeleton.

--- FinalProject/api/dependencies/config.py
+++ FinalProject/api/dependencies/config.py
@@ -1,8 +1,8 @@
 class conf:
     db_host = "localhost"
-    db_name = "sandwich_maker_api"
+    db_name = "sandwhich_maker_api"
     db_port = 3306
     db_user = "root"
-    db_password = "rootroot"
+    db_password = "137909Alexis145!"
     app_host = "localhost"
     app_port = 8000
